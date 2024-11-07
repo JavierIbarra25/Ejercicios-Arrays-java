634e5a33288770ac2f5ea10757d038b949fcb241
Javier_Ibarra <javiermaria.s463101@cesurformacion.com>
2024-11-07 11:37:18 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int contador = 0;
+        int SumPositivos = 0;
+        int SumNegativos = 0;
+        int contadorPositivos = 0;
+        int contadorNegativos = 0;
+
+        Scanner input = new Scanner (System.in);
+
+        System.out.println("Introduce el número total de números de los que quieres calcular la media");
+        int n = input.nextInt();
+
+        int [] numeros = new int [n];
+
+        for (int i = 0; i < numeros.length; i++){
+            System.out.println("Introduce un número: ");
+            numeros [i] = input.nextInt();
+
+            if (numeros [i] == 0){
+                contador++;
+            }
+            else if (numeros [i] > 0){
+                SumPositivos = SumPositivos + numeros[i];
+                contadorPositivos++;
+            }
+            else{
+                SumNegativos = SumNegativos + numeros[i];
+                contadorNegativos++;
+            }
+        }
+
+        if (contadorPositivos == 0){
+            System.out.println("imposible de realizar la media de los positivos");
+        }else{
+            double PositivoMedia = (double) SumPositivos / contadorPositivos;
+            System.out.println("la media de números positivos es " + PositivoMedia);
+        }
+
+        if (contadorNegativos == 0){
+            System.out.println("imposible de realizar la media de los negativos");
+        }else{
+            double NegativoMedia = (double) SumNegativos / contadorNegativos;
+            System.out.println("la media de números negativos es " + NegativoMedia);
+        }
+
+        System.out.println("El número de ceros es + " + contador);
+
+    }
+}
