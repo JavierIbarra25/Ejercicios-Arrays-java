6a421270d5384787fbc86a55e88f264d3a05e3e9
Javier_Ibarra <javiermaria.s463101@cesurformacion.com>
2024-11-14 12:11:40 +0100

    Add files via upload

---
+++ MatrizDeMatrices.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MatrizDeMatrices {
+    public static void main (String [] args){
+        int solve = 1;
+
+        int[][] matriz = {};
+         for (int i = 0; i<matriz.length; i++){
+             for (int j = 0; i< matriz.length; i++){
+                 System.out.println(matriz[i][j]);
+             }
+         }
+
+
+
+    }
+}
